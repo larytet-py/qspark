f871b98e1a4d71235c09952c8b2c3b964e2ee339
arkady <>
2024-10-29 17:55:07 +0200

    bug fix

--- pl.py
+++ pl.py
@@ -3,10 +3,11 @@ from typing import Dict
 from decimal import Decimal
 
 class Position:
-    def __init__(self, stock: str):
+    def __init__(self, stock: str, tick_size: float):
         self.stock = stock
         self.size = 0
         self.realized_pnl = 0.0
+        self.tick_size = tick_size
 
         # FIFO queue for buys
         self.buys = deque()  
@@ -31,7 +32,7 @@ class Position:
             match_qty = min(sell_qty, buy_qty)
             
             # Calculate realized P&L for the matched quantity
-            realized_pnl += match_qty * (price - buy_price)
+            realized_pnl += self.from_tick(match_qty * (self.to_tick(price) - self.to_tick(buy_price)))
             print(f"match_qty={match_qty}, sell_price={price}, buy_price={buy_price}, {price - buy_price}, realized_pnl={realized_pnl}")
             
             # Update remaining quantities
@@ -45,7 +46,13 @@ class Position:
         self.size -= quantity
         self.realized_pnl = realized_pnl
         print(f"Sell {self.stock} {quantity}, {price}, {self.buys}, {self.realized_pnl}")
-    
+
+    def to_tick(self, v: float) -> int:
+        return round(v*self.tick_size)
+
+    def from_tick(self, v: int) -> float:
+        return v/self.tick_size
+
 
 positions: Dict[str, Position] = {}
 
@@ -62,11 +69,11 @@ def calc_pnl(trade: dict):
 
 
 trades = [
-    {"stock": "AAPL", "side": "Buy", "quantity": 100, "price": 220.90},
-    {"stock": "AAPL", "side": "Buy", "quantity": 200, "price": 221.05},
-    {"stock": "AAPL", "side": "Sell", "quantity": 50, "price": 221.10},
-    {"stock": "AAPL", "side": "Sell", "quantity": 100, "price": 221.00},
-    {"stock": "IBM", "side": "Buy", "quantity": 100, "price": 203.50},
+    {"stock": "AAPL", "side": "Buy", "quantity": 100, "price": 220.90, "tick_size": 100},
+    {"stock": "AAPL", "side": "Buy", "quantity": 200, "price": 221.05, "tick_size": 100},
+    {"stock": "AAPL", "side": "Sell", "quantity": 50, "price": 221.10, "tick_size": 100},
+    {"stock": "AAPL", "side": "Sell", "quantity": 100, "price": 221.00, "tick_size": 100},
+    {"stock": "IBM", "side": "Buy", "quantity": 100, "price": 203.50, "tick_size": 100},
 ]
 
 for trade in trades:
